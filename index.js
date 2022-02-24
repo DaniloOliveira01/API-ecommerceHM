6fe590817e6a663b2821626b09c7b59c7d3a217c
Danilo Oliveira <oliveira.83555@gmail.com>
2022-02-23 23:09:20 -0300

    arquivo main

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const userRouter = require("./routes/user")
+
+dotenv.config();
+
+mongoose.connect(
+  process.env.MONGODB_URL
+)
+.then(() => console.log('DBConnection Succefull!!!'))
+.catch((err) => {
+  console.log(err);
+});
+
+app.use('/api/user', userRouter)
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log('backend')
+})
